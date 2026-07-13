54e8bb2efb3336cb117cb5001e467649cf2e8d1a
YASIR IRSHAD <yasir.blackzero@gmail.com>
2026-07-13 16:43:04 +0500

    update

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,6 +4,6 @@ export default function robots() {
       userAgent: `*`,
       allow: `/`,
     },
-    sitemap: `https://www.qeemat.com/sitemap.xml`,
+    sitemap: `https://www.qemaat.com/sitemap.xml`,
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,5 +1,5 @@
 export default function sitemap() {
-  const baseUrl = `https://www.qeemat.com`;
+  const baseUrl = `https://www.qemaat.com`;
 
   return [
     {
